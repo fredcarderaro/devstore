dc8ad57525ccd3e42d44d82926c4b9e8f8aa2a7d
Fred Carderaro <fredcarderaro@gmail.com>
2025-03-17 21:17:20 -0300

    install and configure t3-env for env variables

--- src/env.ts
+++ src/env.ts
@@ -1,23 +1,17 @@
+import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
-/* Constante envSchema define o schema que edevem estar as variáveis ambiente, um objeto e conter a propriedade NEXT_PUBLIC_API_BASE_URL, com valor validado pelo zod, string com formato de url */
-const envSchema = z.object({
-  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
-  APP_URL: z.string().url(),
+export const env = createEnv({
+  server: {
+    APP_URL: z.string().url(),
+  },
+
+  client: {
+    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+  },
+
+  runtimeEnv: {
+    APP_URL: process.env.APP_URL,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
 })
-
-/* Constante parsedEnv recebe o resultado da validação do process.env (variáveis ambiente) com o schema criado */
-const parsedEnv = envSchema.safeParse(process.env)
-
-/* Se houver falha na validação, imprime erro no console e lança um evento de erro  */
-if (!parsedEnv.success) {
-  console.error(
-    'Invalid enviroment variables',
-    parsedEnv.error.flatten().fieldErrors,
-  )
-
-  throw new Error('Invalid enviroment variables.')
-}
-
-/* Se validado com sucesso, exporta a variável ambiente. */
-export const env = parsedEnv.data
